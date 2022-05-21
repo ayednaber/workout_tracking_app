92f33e22952a2fb6f5b0fb5fe67b5d9889175d63
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-05-21 08:03:58 -0400

    Put main functionality in a function (cleaned code)

--- gym_tracking.py
+++ gym_tracking.py
@@ -10,62 +10,40 @@ writerPull = csv.writer(pull)
 legs = open("../../Desktop/Online Work/Gym_Tracking/Legs.csv", 'w', newline='')
 writerLegs = csv.writer(legs)
 
+def writeWorkouts(workoutType):
+    for workout in workoutType:
+            no_of_sets = input("Number of sets for " + workout + ": ")
+            if no_of_sets == "s":
+                continue
+            weight_output = no_of_sets + " sets ("
+            rep_output = ""
+            weights_played = input("Enter your weights separated by a space: ")
+            reps_played = input("Enter your reps separated by a space: ")
+            w = weights_played.split(" ")
+            r = reps_played.split(" ")
+            for j in range(len(w)):
+                if j != int(no_of_sets) - 1:
+                    weight_output += w[j] + ", "
+                    rep_output += r[j] + ", "
+                else:
+                    weight_output += w[j] + ")"
+                    rep_output += r[j]
+
+            if (workoutType == pushWorkouts):
+                writerPush.writerow([workout, weight_output, rep_output])
+            elif (workoutType == pullWorkouts):
+                writerPull.writerow([workout, weight_output, rep_output])
+            else:
+                writerLegs.writerow([workout, weight_output, rep_output])
+
 workoutType = input("Choose your workout: ")
 
 while workoutType != "end":
     if workoutType == "push":
-        for workout in pushWorkouts:
-            no_of_sets = input("Number of sets for " + workout + ": ")
-            weight_output = no_of_sets + " sets ("
-            rep_output = ""
-            weights_played = input("Enter your weights separated by a space: ")
-            reps_played = input("Enter your reps separated by a space: ")
-            w = weights_played.split(" ")
-            r = reps_played.split(" ")
-            for j in range(len(w)):
-                if j != int(no_of_sets) - 1:
-                    weight_output += w[j] + ", "
-                    rep_output += r[j] + ", "
-                else:
-                    weight_output += w[j] + ")"
-                    rep_output += r[j]
-
-            writerPush.writerow([workout, weight_output, rep_output])
-
+        writeWorkouts(pushWorkouts)
     elif workoutType == "pull":
-        for workout in pullWorkouts:
-            no_of_sets = input("Number of sets for " + workout + ": ")
-            weight_output = no_of_sets + " sets ("
-            rep_output = ""
-            weights_played = input("Enter your weights separated by a space: ")
-            reps_played = input("Enter your reps separated by a space: ")
-            w = weights_played.split(" ")
-            r = reps_played.split(" ")
-            for j in range(len(w)):
-                if j != int(no_of_sets) - 1:
-                    weight_output += w[j] + ", "
-                    rep_output += r[j] + ", "
-                else:
-                    weight_output += w[j] + ")"
-                    rep_output += r[j]
-
-            writerPull.writerow([workout, weight_output, rep_output])
+        writeWorkouts(pullWorkouts)
     else:
-        for workout in legWorkouts:
-            no_of_sets = input("Number of sets for " + workout + ": ")
-            weight_output = no_of_sets + " sets ("
-            rep_output = ""
-            weights_played = input("Enter your weights separated by a space: ")
-            reps_played = input("Enter your reps separated by a space: ")
-            w = weights_played.split(" ")
-            r = reps_played.split(" ")
-            for j in range(len(w)):
-                if j != int(no_of_sets) - 1:
-                    weight_output += w[j] + ", "
-                    rep_output += r[j] + ", "
-                else:
-                    weight_output += w[j] + ")"
-                    rep_output += r[j]
-
-            writerLegs.writerow([workout, weight_output, rep_output])
+        writeWorkouts(legWorkouts)
     workoutType = input("Enter another workout type or type `end` to finish: ")
+
